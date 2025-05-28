98a8fd91674be1f00a94aa4ca22ce8ac751d1580
SirBlessing <olaniyiolawale014@gmail.com>
2025-05-28 19:07:20 +0100

    fixed error

--- src/pages/Reciepientpage.js
+++ src/pages/Reciepientpage.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+// import { useState } from "react";
+import React from 'react';
 import "../App.css"; 
 import Availabledonorcard from "../components/Availabledonorcard";
 import FormWithMap from '../components/FormWithMap';
